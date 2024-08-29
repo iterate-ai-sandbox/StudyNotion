ab224d7e22c3f01f777afdc427638cc21a98b1e8
Shubhojeet Bera <shubhojeet.bera@iterate-ai.com>
2024-08-29 20:18:18 +0530

    updates

--- src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
+++ src/components/core/Dashboard/InstructorDashboard/Instructor.jsx
@@ -97,25 +97,25 @@ export default function Instructor() {
               </div>
               <div className="my-4 flex items-start space-x-6">
                 {courses.slice(0, 3).map((course) => (
-                  <div key={course._id} className="w-1/3">
+                  <div key={course?._id} className="w-1/3">
                     <img
-                      src={course.thumbnail}
-                      alt={course.courseName}
+                      src={course?.thumbnail}
+                      alt={course?.courseName}
                       className="h-[201px] w-full rounded-md object-cover"
                     />
                     <div className="mt-3 w-full">
                       <p className="text-sm font-medium text-richblack-50">
-                        {course.courseName}
+                        {course?.courseName}
                       </p>
                       <div className="mt-1 flex items-center space-x-2">
                         <p className="text-xs font-medium text-richblack-300">
-                          {course.studentsEnroled.length} students
+                          {course?.studentsEnroled?.length} students
                         </p>
                         <p className="text-xs font-medium text-richblack-300">
                           |
                         </p>
                         <p className="text-xs font-medium text-richblack-300">
-                          Rs. {course.price}
+                          Rs. {course?.price}
                         </p>
                       </div>
                     </div>
